bfb8b171788e31419ecdce514a3c1fc91fdb784d
BoxCollider <q.roze@rubika-edu.com>
2018-11-14 23:10:18 +0100

    Modified Script Pinata

--- BirthdayPartyGame/Assets/Scripts/Objects/Pinata.cs
+++ BirthdayPartyGame/Assets/Scripts/Objects/Pinata.cs
@@ -2,18 +2,49 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Pinata : Interactable {
+public class Pinata : Interactable
+{
 
-	public GameObject containedObject;
+    LineRenderer line;
+    SpringJoint joint;
+
+    public GameObject containedObject;
+    public bool linked;
+
     public GameObject confettiParticlesPrefab;
 
-	public override void Die()
-	{
+    private void Start()
+    {
+        if (linked)
+        {
+            line = GetComponent<LineRenderer>();
+            joint = GetComponent<SpringJoint>();
+            line.positionCount = 2;
+        }
+    }
+
+    private void Update()
+    {
+        if (linked)
+        {
+            line.SetPosition(0, self.TransformPoint(joint.anchor));
+            line.SetPosition(1, joint.connectedBody.position + joint.connectedAnchor);
+        }
+    }
+
+    public override void Die()
+    {
         GameObject _confettiParticlesRef = Instantiate(confettiParticlesPrefab, transform.position, Quaternion.Euler(-90, 0, 0));
         Destroy(_confettiParticlesRef, 2.5f);
-		base.Die();
-		if (containedObject != null)
-			Instantiate(containedObject, transform.position, Quaternion.identity);
-	}
+        base.Die();
+        if (containedObject != null)
+            Instantiate(containedObject, transform.position, Quaternion.identity);
+    }
+
+    private void OnJointBreak(float breakForce)
+    {
+        line.positionCount = 0;
+        line.enabled = true;
+    }
 
 }
