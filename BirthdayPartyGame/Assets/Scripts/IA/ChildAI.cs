543f2dd564ef64aeea52f2b976accf363e72878a
a.ledouble <a.ledouble@rubika-edu.com>
2018-11-14 14:58:27 +0100

    Import package IA + Menu

---
+++ BirthdayPartyGame/Assets/Scripts/IA/ChildAI.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChildAI : MonoBehaviour {
+	public GameObject player;
+	public GameObject GetPlayer()
+	{
+		return player;
+	}
+
+ 	UnityEngine.AI.NavMeshAgent agent;
+
+	Animator anim;
+
+	bool sad;
+
+	
+
+
+	// Use this for initialization
+	void Start () {
+		agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+		anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		anim.SetBool("sad", sad);
+
+		if (Input.GetKeyDown(KeyCode.B)){
+			sad = true;
+		}
+	}
+}
---
+++ BirthdayPartyGame/Assets/Scripts/IA/NPCBaseFSM.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCBaseFSM : StateMachineBehaviour {
+
+	public GameObject NPC;
+	public GameObject player;
+
+	public Rigidbody rb;
+
+	public UnityEngine.AI.NavMeshAgent agent;
+
+	public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex){
+		NPC = animator.gameObject;
+		player = NPC.GetComponent<ChildAI>().GetPlayer();
+		agent = NPC.GetComponent<UnityEngine.AI.NavMeshAgent>();
+		rb = NPC.GetComponent<Rigidbody>();
+	}
+}
