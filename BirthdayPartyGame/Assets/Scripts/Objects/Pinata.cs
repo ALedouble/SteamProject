1b0227bf9d8b24926f23b8aef6d0ca67fa030dd0
BoxCollider <q.roze@rubika-edu.com>
2018-11-14 22:59:18 +0100

    Victory Scene created & Confetti particle integrated

--- BirthdayPartyGame/Assets/Scripts/Objects/Pinata.cs
+++ BirthdayPartyGame/Assets/Scripts/Objects/Pinata.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class Pinata : Interactable {
 
 	public GameObject containedObject;
+    public GameObject confettiParticlesPrefab;
 
 	public override void Die()
 	{
+        GameObject _confettiParticlesRef = Instantiate(confettiParticlesPrefab, transform.position, Quaternion.Euler(-90, 0, 0));
+        Destroy(_confettiParticlesRef, 2.5f);
 		base.Die();
-
 		if (containedObject != null)
 			Instantiate(containedObject, transform.position, Quaternion.identity);
 	}
