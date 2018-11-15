c3deb0cf716b3937195d817eca4979f57e654581
BoxCollider <q.roze@rubika-edu.com>
2018-11-15 10:55:07 +0100

    Integrated wood pieces particles when breaking fences

---
+++ BirthdayPartyGame/Assets/Scripts/Objects/Fence.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fence : Interactable {
+
+    public GameObject woodPiecesParticlesPrefab;
+
+    public override void Die()
+    {
+        GameObject _woodPiecesParticlesRef = Instantiate(woodPiecesParticlesPrefab, transform.position+Vector3.up*1.7f, Quaternion.identity);
+        Destroy(_woodPiecesParticlesRef, 1);
+        base.Die();
+    }
+}
