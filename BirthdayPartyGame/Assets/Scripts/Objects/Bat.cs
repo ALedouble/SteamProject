3e3f27383df9717c3cdf99b3d88aeeadacc2736b
Alessandro <allezcheznice@gmail.com>
2018-11-27 12:16:13 +0100

    Held objects constrained instead of kinematic; Trees now spread fire;

--- BirthdayPartyGame/Assets/Scripts/Objects/Bat.cs
+++ BirthdayPartyGame/Assets/Scripts/Objects/Bat.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class Bat : Interactable
 {
-
 	public float swingDuration = 1;
     public AudioSource myAudioSource;
     public AudioClip batteSwingClip;
@@ -12,32 +11,15 @@ public class Bat : Interactable
 	public override void Activate()
 	{
 		base.Activate();
-		//StartCoroutine(BatSwing());
 		canBreak = true;
 		canActivate = false;
-		print("Start swinging");
 		myAudioSource.PlayOneShot(batteSwingClip);
 	}
 
 	public override void Deactivate()
 	{
 		base.Deactivate();
-		print("Stop swinging");
 		canBreak = false;
 		canActivate = true;
 	}
-
-	//IEnumerator BatSwing()
-	//{
-	//	canBreak = true;
-	//	canActivate = false;
-	//	print("Start swinging");
-	//       myAudioSource.PlayOneShot(batteSwingClip);
-
-	//       yield return new WaitForSeconds(swingDuration);
-
-	//	canBreak = false;
-	//	print("Stop swinging");
-	//	canActivate = true;
-	//}
 }
