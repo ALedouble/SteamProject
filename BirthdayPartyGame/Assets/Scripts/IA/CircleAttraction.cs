d0f9b4a016428fd5674b05c9d0524ba931cf6828
a.ledouble <a.ledouble@rubika-edu.com>
2018-12-08 10:59:22 +0100

    IA attraction end

--- BirthdayPartyGame/Assets/Scripts/IA/CircleAttraction.cs
+++ BirthdayPartyGame/Assets/Scripts/IA/CircleAttraction.cs
@@ -7,6 +7,7 @@ public class CircleAttraction : MonoBehaviour {
 	public float radius;
 	public GameObject[] allPOI;
 	public int valueCircle;
+	public bool inCircle = false;
 
 	public Vector3 DirFromAngle(float angleDegrees){
 		return new Vector3(Mathf.Sin(angleDegrees * Mathf.Deg2Rad), 0, Mathf.Cos(angleDegrees = Mathf.Deg2Rad));
--- BirthdayPartyGame/Assets/Scripts/IA/OnTheWayScript.cs
+++ BirthdayPartyGame/Assets/Scripts/IA/OnTheWayScript.cs
@@ -8,15 +8,12 @@ public class OnTheWayScript : NPCBaseFSM {
 	CircleAttraction[] ca;	
 	ChildAI[] child;
 	float IdleTime = 0;
-	bool inRadius = false;
-
 	public float animDistance;
-
 	public float cooldown;
-
 	int maxValue;
+	int maxValueGlobal;
+	public bool inCircle;
 
-	int circleNumber;
 	
 
 	void Awake() {
@@ -32,179 +29,76 @@ public class OnTheWayScript : NPCBaseFSM {
 
 	// OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
 	override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-	
-		for (int i = 0; i < ca.Length; i++){
 
-			if(ca[i].valueCircle > maxValue){
+		for (int i = 0; i < ca.Length; i++){
+			float distance = Vector3.Distance(agent.transform.position, ca[i].transform.position); // Check la distance entre l'IA est les cercles d'attractions.
+			
+			Debug.Log(maxValue);
+
+			if(ca[i].valueCircle > maxValue) // Change la valeur du cercle d'attraction en cas de valeur plus grande
+			{
 				maxValue = ca[i].valueCircle;
 			}
-			
-			/* if(ca[i].valueCircle < maxValue){
-				maxValue = 0;
-			}
-			*/
-			float dist = Vector3.Distance(agent.transform.position, ca[i].transform.position);
 
-			Debug.Log(inRadius);
 
-			if(dist <= ca[i].radius)  
+			if (ca[i].valueCircle == maxValue) // Quand la valeur est la plus grande et que l'ia s'est donc dirigé vers le cercle avec la plus grande valeur
 			{
-				inRadius = true;	
-			}
-			else {
-				inRadius = false;
-			}
+				if (distance <= ca[i].radius) // Si l'IA est dans un cercle d'attraction alors : 
+				{
+					Debug.Log(maxValue);
+					inCircle = true; // Active un booléen 
+					cooldown = 0;
 
-			if (inRadius == true){
-				IdleTime += Time.deltaTime;
-				MoveToPOI();
-				agent.speed = 3.5f;
-				circleNumber = i;
-				break;
-			} else {
+					float distanceClosestPoint = Mathf.Infinity; 
+
+					for(int y = 0; y < ca[i].allPOI.Length; y++){ // Check tout les POI du cercle d'attraction
+						float distanceToPOI = Vector3.Distance(agent.transform.position, ca[i].allPOI[y].transform.position); // Check distance entre IA et les POI
+
+						if (distanceToPOI < distanceClosestPoint) // Si la distance est plus petite alors l'IA se dirige vers ce POI
+						{
+							distanceClosestPoint = distanceToPOI;
+							agent.SetDestination(ca[i].allPOI[y].transform.position);
+							Debug.DrawLine(NPC.transform.position, ca[i].allPOI[y].transform.position);
+
+							animDistance = Vector3.Distance(NPC.transform.position, ca[i].allPOI[y].transform.position);
+							anim.SetFloat("distance", Vector3.Distance(NPC.transform.position, ca[i].allPOI[y].transform.position));
+						}
+					} 	
+				}
+			}
+			else if (distance >  ca[i].radius)
+			{
+				Debug.Log("hey wtf");
+				inCircle = false;
 				cooldown -= Time.deltaTime;
 				agent.speed = 0;
 
 				if (cooldown <= 0){
-					MoveToPOI();
 					agent.speed = 3.5f;
+					Debug.Log("oui");
 					cooldown = 0;
-					circleNumber = Random.Range(1, 2);
-				}
 
-				
-			}
-			
-		}
-	}
+					Debug.Log(maxValue);
+					 
 
-	public void MoveToPOI(){
-		for (int i = 0; i < ca.Length; i++){
-			CircleAttraction[] allPOI = GameObject.FindObjectsOfType<CircleAttraction>();
-			agent.SetDestination(allPOI[circleNumber].transform.position);
-
-			animDistance = Vector3.Distance(NPC.transform.position, allPOI[circleNumber].transform.position);
-			anim.SetFloat("distance", Vector3.Distance(NPC.transform.position, allPOI[circleNumber].transform.position));
-		}
-		
+					if (inCircle == false){
+						for (int z = 0; z < ca.Length; z++){
+							if(ca[z].valueCircle > maxValueGlobal) // Change la valeur du cercle d'attraction en cas de valeur plus grande
+							{
+								maxValueGlobal = ca[z].valueCircle;
+							}
 
 
-
-
-
-
-
-
-		/*for (int i = 0; i < ca.Length; i++){
-			float distanceClosestPoint = Mathf.Infinity;
-			CircleAttraction closestPOI = null;
-			CircleAttraction[] allPOI = GameObject.FindObjectsOfType<CircleAttraction>();
-			foreach (CircleAttraction currentPoint in allPOI )
-			{
-				float distanceToPOI = (currentPoint.transform.position - NPC.transform.position).sqrMagnitude;
-				if (distanceToPOI < distanceClosestPoint)
-				{
-					distanceClosestPoint = distanceToPOI;
-					closestPOI = currentPoint;
-					if(agent.enabled == true)
-					{
-						agent.SetDestination(closestPOI.transform.position);
+							if (ca[z].valueCircle == maxValueGlobal) // Quand la valeur est la plus grande et que l'ia s'est donc dirigé vers le cercle avec la plus grande valeur
+							{
+								agent.SetDestination(ca[z].transform.position);
+							}
+						}
 					}
-				anim.SetFloat("distance", Vector3.Distance(NPC.transform.position, closestPOI.transform.position));
-
-				Debug.DrawLine(NPC.transform.position, closestPOI.transform.position);
-				a
+									
 				}
-			}		
-		}
-		*/
+			}
+		}	
 	}
-	 
 }
-		
-		
-
-		/* 
-		for (int i = 0; i < ca.Length; i++){
-			if(ca[i].valueCircle > maxValueCircle){
-				maxValueCircle = ca[i].valueCircle;
-			}
-			
-			if(ca[i].valueCircle < maxValueCircle){
-				maxValueCircle = 0;
-			}
-
-			
-
-			float dist = Vector3.Distance(agent.transform.position, circleNumber[i].transform.position);
-			
-			if (ca[i].valueCircle == maxValueCircle){
-				if (dist > ca[i].radius && agent.enabled == true)
-				{
-					agent.SetDestination(circleNumber[i].transform.position);
-					//Debug.Log(maxValueCircle);
-					Debug.DrawLine(NPC.transform.position, circleNumber[i].transform.position);
-
-					
-					if (Input.GetKeyDown(KeyCode.E)){
-						maxValueCircle = 0;
-						ca[i].valueCircle = -10;
-					}	
-					
-
-					if(dist <= ca[i].radius) {
-						MoveToPOI();
-					}
-				}
-
-
-				if(dist <= ca[i].radius) {
-						MoveToPOI();
-				}
-			}
-
-
-			*/
-			
-		
-
-		//Debug.Log(maxValueCircle);
-	 
 	
-		
-
-
-
-	//	for (int i = 0; i < pointsOfInterests.Length; i++){
-			/* float distPOI = Vector3.Distance(agent.transform.position, pointsOfInterests[i].transform.position);
-			if(distPOI < minDistPOI){
-				minDistPOI = distPOI;
-				agent.SetDestination(pointsOfInterests[i].transform.position);
-
-				anim.SetFloat("distance", Vector3.Distance(NPC.transform.position, pointsOfInterests[i].transform.position));
-			}
-
-
-			
-
-
-
-	//OnStateExit is called when a transition ends and the state machine finishes evaluating this state
-	override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-	
-	}
-
-
-
-	// OnStateMove is called right after Animator.OnAnimatorMove(). Code that processes and affects root motion should be implemented here
-	override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-	//
-	//}
-
-	// OnStateIK is called right after Animator.OnAnimatorIK(). Code that sets up animation IK (inverse kinematics) should be implemented here.
-	//override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-	//
-	//}
-
-	*/
-
