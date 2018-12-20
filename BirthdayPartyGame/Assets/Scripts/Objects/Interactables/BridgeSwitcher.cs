2d2792d9b1221cf79f73960f84a8007a7ff53dc6
BoxCollider <q.roze@rubika-edu.com>
2018-12-20 11:42:40 +0100

    barrierSwitch integrated & lawnmower anim done

---
+++ BirthdayPartyGame/Assets/Scripts/Objects/Interactables/BridgeSwitcher.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BridgeSwitcher : Interactable {
+
+    public Animator bridgeBarrierAnim;
+
+    protected override void Start()
+    {
+        base.Start();
+        print("prout");
+    }
+
+    public override void Activate()
+    {
+        print("prout");
+        base.Activate();
+        if (!activated)
+        {
+            bridgeBarrierAnim.SetTrigger("ToggleTrigger");
+        }
+        else
+        {
+            Deactivate();
+        }
+    }
+
+    public override void Deactivate()
+    {
+        base.Deactivate();
+        bridgeBarrierAnim.SetTrigger("ToggleTrigger");
+    }
+}
