4a99088d42534c0223e8d4f4ddeced8499215b4c
BoxCollider <q.roze@rubika-edu.com>
2019-01-10 21:41:30 +0100

    Creation of new Level In Quentin's LD Folder

---
+++ BirthdayPartyGame/Assets/Scripts/Objects/SprayWaterMachine.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SprayWaterMachine : MonoBehaviour {
+
+    public GameObject waterSpherePrefab;
+    public Transform spawnPoint;
+    
+	void Start () {
+        StartCoroutine(SprayWater());
+	}
+
+    IEnumerator SprayWater()
+    {
+        GameObject gameObjectRef = Instantiate(waterSpherePrefab, spawnPoint.position, spawnPoint.rotation);
+        gameObjectRef.GetComponent<Rigidbody>().AddForce(500 * spawnPoint.forward + 100 * spawnPoint.up);
+        Destroy(gameObjectRef, 1f);
+        yield return new WaitForSeconds(0.05f);
+        StartCoroutine(SprayWater());
+    }
+}
