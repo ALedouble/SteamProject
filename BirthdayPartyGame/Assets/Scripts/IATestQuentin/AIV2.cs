94632e606810a7eabd4190f79250148d3470e8b5
Lug <l.ambroise@rubika-edu.com>
2019-01-13 17:23:11 +0100

    New AIs added

--- BirthdayPartyGame/Assets/Scripts/IATestQuentin/AIV2.cs
+++ BirthdayPartyGame/Assets/Scripts/IATestQuentin/AIV2.cs
@@ -9,6 +9,7 @@ public class AIV2 : MonoBehaviour {
 
     [Space]
     [Header("READ-ONLY")]
+    public AISpots currentSpot;
     public List<AttractionCircleV2> AttractionCircleList = new List<AttractionCircleV2>();
     public AIState myState = AIState.Neutral;
 
@@ -18,17 +19,12 @@ public class AIV2 : MonoBehaviour {
 
     [Space]
     [Header("Referencies")]
-    public Renderer myRenderer;
+    public Animator myAnim;
     public NavMeshAgent myNavMeshAgent;
-    public Material redMat;
-    public Material whiteMat;
-    public Material greenMat;
-    public Material yellowMat;
 
 
     float cooldownInDistress;
     AttractionCircleV2 currentAttractionCircle;
-    AISpots currentSpot;
     AttractionCircleV2 attractionCircleFleeing;
 
 
@@ -41,7 +37,7 @@ public class AIV2 : MonoBehaviour {
 
     public void RemoveAttractionCircle(AttractionCircleV2 _oldAttractionCircle)
     {
-        if(myState == AIState.Fleeing && _oldAttractionCircle == attractionCircleFleeing)
+        if(myState == AIState.Fleeing && _oldAttractionCircle == attractionCircleFleeing) //si le cercle dont on vient de sortir était le repulse
         {
             SetState(AIState.Neutral);
         }
@@ -55,22 +51,26 @@ public class AIV2 : MonoBehaviour {
         int maxScore = 0;
         for (int i = 0; i < AttractionCircleList.Count; i++)
         {
-            if (AttractionCircleList[i].repulse)
+            if (AttractionCircleList[i].repulse) // one repulse circle detected !!
             {
                 attractionCircleFleeing = AttractionCircleList[i];
                 SetState(AIState.Fleeing);
-                print("flee!!");
                 return;
             }
-            if (AttractionCircleList[i].score > maxScore)
+            if (AttractionCircleList[i].score > maxScore) //attraction circle better than previous ones checked
             {
                 _targetedAttractionCircle = AttractionCircleList[i];
                 maxScore = _targetedAttractionCircle.score;
             }
         }
-        if (maxScore > 0 && _targetedAttractionCircle!=null)
+        if (maxScore > 0 && _targetedAttractionCircle!=null && myState!=AIState.Fleeing) //new attraction circle found
         {
             SetNewCurrentAttractionCircle(_targetedAttractionCircle);
+        }
+        else if(myState != AIState.Fleeing) //when no attraction circles
+        {
+            SetState(AIState.Neutral);
+            SetNewCurrentAttractionCircle(null);
         }
     }
 
@@ -85,20 +85,37 @@ public class AIV2 : MonoBehaviour {
     {
         if ((myState == AIState.Neutral || myState == AIState.Amused) && currentAttractionCircle != null)
         {
+            float distanceMin = Mathf.Infinity;
+            AISpots newSpot = null;
             for (int i = 0; i < currentAttractionCircle.mySpots.Length; i++)
             {
                 if (currentAttractionCircle.mySpots[i].availability)
                 {
                     if (currentSpot != null)
                     {
-                        currentSpot.availability = true;
+                         //make it available when you leave the spot
                     }
-                    currentSpot = currentAttractionCircle.mySpots[i];
-                    currentSpot.availability = false;
-                    return;
+                    if(Vector3.Distance(transform.position, currentAttractionCircle.mySpots[i].transform.position) < distanceMin)
+                    {
+                        distanceMin = Vector3.Distance(transform.position, currentAttractionCircle.mySpots[i].transform.position);
+                        newSpot = currentAttractionCircle.mySpots[i];
+                    }
                 }
             }
-
+            if(currentSpot != null)
+            {
+                currentSpot.availability = true;           
+            }
+            if (newSpot != null)
+            {
+                currentSpot = newSpot;
+                currentSpot.availability = false;
+            }
+        }
+        else
+        {
+            currentSpot.availability = true;
+            currentSpot = null;
         }
     }
 
@@ -107,17 +124,16 @@ public class AIV2 : MonoBehaviour {
         if ((myState == AIState.Neutral || myState == AIState.Amused) && currentSpot!= null)
         {
             myNavMeshAgent.SetDestination(currentSpot.transform.position);
-            SetState(AIState.Neutral);
+            if(myNavMeshAgent.remainingDistance > 0.5f)
+            {
+                SetState(AIState.Neutral);
+            }
         }
     }
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.P))
-        {
-            SetState(AIState.inDistress);
-        }
-
+        myAnim.SetFloat("MoveSpeed", myNavMeshAgent.velocity.magnitude);
         switch (myState)
         {
             case AIState.inDistress:
@@ -128,9 +144,14 @@ public class AIV2 : MonoBehaviour {
                 }
                 break;
             case AIState.Amused:
+                if(myNavMeshAgent.remainingDistance > 0.5f)
+                {
+                    SetState(AIState.Neutral);
+                    print("heyyyy");
+                }
                 break;
             case AIState.Neutral:
-                if(currentSpot != null && myNavMeshAgent.remainingDistance < 0.5f)
+                if(currentSpot != null && myNavMeshAgent.remainingDistance <= 0.5f)
                 {
                     SetState(AIState.Amused);
                 }
@@ -147,23 +168,29 @@ public class AIV2 : MonoBehaviour {
             case AIState.inDistress:
                 myState = AIState.inDistress;
                 cooldownInDistress = cooldownInDistressValue;
-                myRenderer.material = redMat;
+                myAnim.SetTrigger("SurpriseTrigger");
                 ResetAttractionCirclesAndSpots();
                 myNavMeshAgent.SetDestination(transform.position);
                 break;
             case AIState.Amused:
                 myState = AIState.Amused;
-                myRenderer.material = greenMat;
+                myAnim.SetTrigger("AmusedTrigger");
                 break;
             case AIState.Neutral:
                 myState = AIState.Neutral;
-                myRenderer.material = whiteMat;
+                myAnim.SetTrigger("NeutralTrigger");
                 break;
             case AIState.Fleeing:
                 myState = AIState.Fleeing;
-                myRenderer.material = yellowMat;
+                myAnim.SetTrigger("FleeingTrigger");
                 ResetAttractionCirclesAndSpots();
-                Vector3 newDestination = (transform.position - attractionCircleFleeing.transform.position).normalized * (attractionCircleFleeing.GetComponent<SphereCollider>().radius+1);
+
+                //Set the fleeing destination
+                Vector3 newDestination = (transform.position - attractionCircleFleeing.transform.position).normalized;
+                float distance = attractionCircleFleeing.GetComponent<SphereCollider>().radius /2 +1;
+                newDestination *= distance;
+                newDestination.y = transform.position.y;
+                newDestination += transform.position;
                 myNavMeshAgent.SetDestination(newDestination);
                 break;
         }
