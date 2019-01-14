3f56fcdb2107a262732580f71bb318cad55a981f
BoxCollider <q.roze@rubika-edu.com>
2019-01-14 10:23:06 +0100

    Update Attraction Circle Script & Prefab + Pinata Scripts & PRefab

--- BirthdayPartyGame/Assets/Scripts/IATestQuentin/AttractionCircleV2.cs
+++ BirthdayPartyGame/Assets/Scripts/IATestQuentin/AttractionCircleV2.cs
@@ -4,11 +4,19 @@ using UnityEngine;
 
 public class AttractionCircleV2 : MonoBehaviour {
 
-    List<AIV2> AIsInRange = new List<AIV2>();
+    [Space]
+    [Header("Referencies")]
+    public SphereCollider mySphereCollider;
+
+    [Space]
+    [Header("Values to Tweak")]
     public int score;
     public AISpots[] mySpots;
+
     [HideInInspector]
     public bool repulse;
+    Vector3 latestPosition;
+    List<AIV2> AIsInRange = new List<AIV2>();
 
     private void OnTriggerEnter(Collider other)
     {
@@ -39,12 +47,29 @@ public class AttractionCircleV2 : MonoBehaviour {
         }
     }
 
-    void ChangeRepulse(bool _doesItRepulse)
+    public void ChangeRepulse(bool _doesItRepulse)
     {
         repulse = _doesItRepulse;
         for (int i = 0; i < AIsInRange.Count; i++)
         {
             AIsInRange[i].CompareAttractionCircles();
         }
+    }
+
+    public void ChangeRadius(float _newRadius)
+    {
+        mySphereCollider.radius = _newRadius;
+    }
+
+    private void Update()
+    {
+        if(transform.position != latestPosition)
+        {
+            for (int i = 0; i < AIsInRange.Count; i++)
+            {
+                AIsInRange[i].CompareAttractionCircles();
+            }
+        }
+        latestPosition = transform.position;
     }
 }
