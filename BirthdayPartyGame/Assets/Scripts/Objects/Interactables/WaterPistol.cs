397d7f7c3c90f36c8bafb39c71891b7b63a670f0
BoxCollider <q.roze@rubika-edu.com>
2019-02-12 14:37:10 +0100

    waterpistolprefab feedback integrated (SFX/VFX)

--- BirthdayPartyGame/Assets/Scripts/Objects/Interactables/WaterPistol.cs
+++ BirthdayPartyGame/Assets/Scripts/Objects/Interactables/WaterPistol.cs
@@ -8,6 +8,9 @@ public class WaterPistol : Interactable {
 	public Transform spawnPoint;
 	public float force = 16;
 	public float shotsInterval = 0.5f;
+    public AudioSource myAudioSource;
+    public AudioClip waterPistolShootClip;
+    public GameObject waterPart;
 
 	public override void Activate()
 	{
@@ -19,6 +22,9 @@ public class WaterPistol : Interactable {
 	{
 		GameObject gameObjectRef1 = Instantiate(waterDrop, spawnPoint.position, self.rotation);
 		gameObjectRef1.GetComponent<Rigidbody>().AddForce(force * self.forward + 1 * Vector3.up);
+        GameObject waterPartRef = Instantiate(waterPart, spawnPoint.position, self.rotation);
+        Destroy(waterPartRef, 0.5f);
+        myAudioSource.PlayOneShot(waterPistolShootClip);
 		canActivate = false;
 		yield return new WaitForSeconds(shotsInterval);
 		canActivate = true;
