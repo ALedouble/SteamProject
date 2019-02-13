e42686e5fbc9d011ff1d14c94275e78aeeb5cacf
BoxCollider <q.roze@rubika-edu.com>
2019-02-13 15:04:37 +0100

    FlowerUpdated + change on Player's Prefab

---
+++ BirthdayPartyGame/Assets/Scripts/Objects/Interactables/Flowers.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flowers : Interactable {
+
+    RowOfFlower myParent;
+
+
+    protected override void Start()
+    {
+        base.Start();
+        myParent = GetComponentInParent<RowOfFlower>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "PlayerFeet")
+        {
+            myParent.ActualizeNbFlower();
+            Die();
+        }
+    }
+}
---
+++ BirthdayPartyGame/Assets/Scripts/Objects/Others/RowOfFlower.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RowOfFlower : MonoBehaviour {
+
+    public int nbFlower;
+    public AttractionCircleV2 myAttractionCircle;
+
+    public void ActualizeNbFlower()
+    {
+        nbFlower--;
+        if (nbFlower <= 0)
+        {
+            myAttractionCircle.ChangeScore(0);
+            Destroy(gameObject);
+        }
+    }
+}
