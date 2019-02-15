445b381d7d7212bf0031d7c63ee1564413b6ad54
BoxCollider <q.roze@rubika-edu.com>
2019-02-15 14:42:16 +0100

    début de fireworkLauncher mais bug bloquant avec le contact du feu

---
+++ BirthdayPartyGame/Assets/Scripts/Objects/Interactables/FireworkLauncher.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireworkLauncher : Interactable {
+
+    public float cooldownMaxValue;
+    float cooldown;
+    public GameObject[] fireworks;
+    int whichFirework;
+
+    protected override void Start()
+    {
+        cooldown = cooldownMaxValue;
+    }
+
+    private void Update()
+    {
+        if (burning && whichFirework<fireworks.Length-1)
+        {
+            if (cooldown > 0)
+            {
+                cooldown -= Time.deltaTime;
+            }
+            else
+            {
+                LaunchFirework();
+                cooldown = cooldownMaxValue;
+                whichFirework++;
+            }
+        }
+    }
+
+    void LaunchFirework()
+    {
+        fireworks[whichFirework].SetActive(false);
+    }
+}
