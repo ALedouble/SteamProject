f53bb618b2d303e52665a36bfc238b55b705047a
BoxCollider <q.roze@rubika-edu.com>
2019-02-15 15:09:16 +0100

    Added sound to the bridge

--- BirthdayPartyGame/Assets/Scripts/Objects/Interactables/ElectricBridge.cs
+++ BirthdayPartyGame/Assets/Scripts/Objects/Interactables/ElectricBridge.cs
@@ -8,18 +8,20 @@ public class ElectricBridge : Interactable {
 	public Animator myAnim;
     public NavMeshObstacle myNavObstacle;
     public BoxCollider myCollider;
+    public AudioSource myAudioSource;
+    public AudioClip risingClip;
+    public AudioClip loweringClip;
 
 	public override void Activate()
 	{
-        print("herehere");
 		base.Activate();		
 		if (!activated)
 		{
-            print("here");
             myAnim.SetBool("UpBool", false);
             myNavObstacle.enabled = false;
             myCollider.enabled = false;
             activated = true;
+            myAudioSource.PlayOneShot(loweringClip);
         }
 	}
 
@@ -30,6 +32,7 @@ public class ElectricBridge : Interactable {
         myNavObstacle.enabled = true;
         myCollider.enabled = true;
         activated = false;
+        myAudioSource.PlayOneShot(risingClip);
     }
 
 }
