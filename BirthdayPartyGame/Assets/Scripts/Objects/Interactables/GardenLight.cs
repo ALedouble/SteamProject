beb574ac22fbd8fe22976b1ec34de95a50ab74bc
BoxCollider <q.roze@rubika-edu.com>
2019-02-16 12:41:48 +0100

    Update Part + Sound on P_Garden_Light

--- BirthdayPartyGame/Assets/Scripts/Objects/Interactables/GardenLight.cs
+++ BirthdayPartyGame/Assets/Scripts/Objects/Interactables/GardenLight.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class GardenLight : Interactable {
 
+    public AudioSource myAudioSource;
+    public AudioClip breakingClip;
+    public GameObject flashPart;
+
     public override void Die()
     {
         if (!electrified)
@@ -11,6 +15,9 @@ public class GardenLight : Interactable {
             GetElectrified(null);
             electricityScript.canSpread = true;
             electricityScript.firstGeneration = true;
+            myAudioSource.PlayOneShot(breakingClip);
+            GameObject flashPartRef = Instantiate(flashPart, transform.position + Vector3.up * 0.8f, Quaternion.identity);
+            Destroy(flashPartRef, .5f);
         }
         //base.Die();
     }
