45cfe15cce541f1fc6c81bc6e694c7d563db3bb8
BoxCollider <q.roze@rubika-edu.com>
2019-02-16 12:27:16 +0100

    GardenLight functionnal + modification LD4 + LD14

---
+++ BirthdayPartyGame/Assets/Scripts/Objects/Interactables/GardenLight.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GardenLight : Interactable {
+
+    public override void Die()
+    {
+        if (!electrified)
+        {
+            GetElectrified(null);
+            electricityScript.canSpread = true;
+            electricityScript.firstGeneration = true;
+        }
+        //base.Die();
+    }
+
+}
