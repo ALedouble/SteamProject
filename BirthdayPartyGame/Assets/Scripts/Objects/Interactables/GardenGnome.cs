df21dad0f7fc9f00c4630a3265ad3733faae667d
BoxCollider <q.roze@rubika-edu.com>
2019-02-16 22:40:02 +0100

    Metal Bat + Gnome done

---
+++ BirthdayPartyGame/Assets/Scripts/Objects/Interactables/GardenGnome.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GardenGnome : Interactable {
+
+    public AudioSource myAudioSource;
+    public AudioClip breakClip;
+
+	public override void Die()
+    {
+        base.Die();
+        myAudioSource.PlayOneShot(breakClip);
+    }
+}
