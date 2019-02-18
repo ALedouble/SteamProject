79a589499f66e412272cc81780544dc3f14b1dcd
BoxCollider <q.roze@rubika-edu.com>
2019-02-18 11:45:07 +0100

    implemented "Sproutch" on cake break & clean LD1 hierarchy

---
+++ BirthdayPartyGame/Assets/Scripts/Objects/Interactables/Cake.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cake : Interactable {
+
+    public AudioClip sproutchClip;
+
+    public override void Die()
+    {
+        GameObject.Find("LevelAudioSource").GetComponent<AudioSource>().PlayOneShot(sproutchClip);
+        base.Die();
+    }
+}
