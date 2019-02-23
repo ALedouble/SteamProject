c8ae0af9401330f0838906a26e0794795d3f4aa9
BoxCollider <q.roze@rubika-edu.com>
2019-02-23 13:46:41 +0100

    integrated some new sounds

---
+++ BirthdayPartyGame/Assets/Scripts/Objects/Interactables/Watermelon.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Watermelon : Interactable {
+
+    public AudioClip sproutchClip;
+
+    public override void Die()
+    {
+        GameObject.Find("LevelAudioSource").GetComponent<AudioSource>().PlayOneShot(sproutchClip);
+        base.Die();
+    }
+}
